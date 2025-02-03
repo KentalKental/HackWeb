375f4efc32df94baa7fc76b2bd2d5edad4e20125
Yosss <YosuaMontolaluXD@gmail.com>
2025-02-03 20:04:50 +0700

    ADDED NAVBAR AND 2 NEW ASSETS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { useState } from 'react'
+import Navbar from './Navbar'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <header>
-        <a>HOME</a>
-
-        <ul>
-          <li><a>Create Hackathon |</a></li> 
-          <li><a>Join Hackathon |</a></li>
-          <li><a>Mentorships</a></li>
-        </ul>
-
-        
-
-
-      </header>
+      <Navbar></Navbar>
     </>
   )
 }
---
+++ src/Navbar.tsx
@@ -0,0 +1,44 @@
+// BOOTSTRAP 5
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// BOOTSTRAP ICONS
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+function Navbar() {
+    return (
+        <nav className="navbar px-3 navbar-expand-lg bg-transparent">
+            <div className="container-fluid d-flex justify-content-between align-items-center">
+
+                {/* Logo / Brand */}
+                <a href="#" className="navbar-brand fw-bold">TRADE</a>
+
+                {/* Navbar Links */}
+                <ul className="navbar-nav d-flex flex-row gap-4 mx-auto fw-bold">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">CREATE HACKATHON</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">JOIN HACKATHON</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">MENTORSHIP</a>
+                    </li>
+                </ul>
+
+                {/* Profile Section */}
+                <div className="profile d-flex align-items-center">
+                    <img
+                        src="..\src\assets\pfp.jpg"
+                        className="img-fluid rounded-circle"
+                        alt="Profile"
+                        style={{ height: "30px", width: "30px", marginRight: "8px" }}
+                    />
+                    <img src="..\src\assets\ArrowPfp.svg" alt="" />
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
