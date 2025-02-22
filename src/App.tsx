5fd5c306eada0e4df0e52095c1ab1118c3eee456
Jonathan The <bc@email.com>
2025-02-22 11:22:25 +0700

    Tambahin dikit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // BOOTSTRAP ICONS
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css'
-import MultiStepForm from './MultiStepForm';
-import Navbar from './Component/Navbar';
-import ProfileBiodata from './Component/ProfileBiodata';
-import OuterGlassContainer from './Component/OuterGlassContainer';
-import InnerGlassContainer from './Component/InnerGlassContainer';
-import Home from './Home';
+import { MentorInformation } from "./MentorInformation"
 function App() {
 
   return (
